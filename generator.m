b85d7c3bca5110406f29ba74002d222a2ed66b1d
Huang Sen <huangsen1993@gmail.com>
2020-11-11 11:16:53 +0800

    update

--- generator.m
+++ generator.m
@@ -1,35 +1,58 @@
 %% Generate emitter and sensors
 %created by Huang Sen
 %Email: huangsen1993@gmail.com
-function [emitter,XYZ,beta0] = generator(M,F,R,Rb,Rm,Ym,max_dis,min_dis)
-emitter = randn(3,1);
-emitter = R*emitter / norm(emitter);
+function [emitter,XYZ,beta0] = generator(M,K,F,R,Rb,Rm,Ym,max_dis,min_dis)
+emitter(1,:) = randn(1,3);
+emitter(1,:) = R*emitter(1,:) / norm(emitter(1,:));
 for i = 1:M
-while(1)
-    XYZ(i,:) = randn(1,3);
-    XYZ(i,:) = R*XYZ(i,:) / norm(XYZ(i,:));
-    if norm(emitter' - XYZ(i,:))<max_dis&norm(emitter' - XYZ(i,:))>min_dis
-        break
+    while(1)
+        XYZ(i,:) = randn(1,3);
+        XYZ(i,:) = R*XYZ(i,:) / norm(XYZ(i,:));
+        if norm(emitter(1,:) - XYZ(i,:))<max_dis&norm(emitter(1,:) - XYZ(i,:))>min_dis
+            break
+        end
     end
 end
+
+
+
+
+for k = 2:K
+    while(1)
+        index = zeros(1,M);
+        emitter(k,:) = randn(1,3);
+        emitter(k,:) = R*emitter(k,:) / norm(emitter(k,:));
+        for i = 1:M
+            if norm(emitter(k,:) - XYZ(i,:))<max_dis&norm(emitter(k,:) - XYZ(i,:))>min_dis
+                index(i) = 1;
+            else
+                break
+            end
+        end
+        if all(index) == 1
+            break
+        end
+    end
 end
+
 %Calculate corresponing beta of x
+for k = 1:K
 while(1)
 beta = zeros(1,M);
-x = emitter';
-for i =1:10
-    for k = 1:M
-        beta = solve_eq(F,R,Rb,Rm,Ym,beta,XYZ,x,k);
-    end
+x = emitter(k,:);
+for i = 1:M
+    beta = solve_eq(F,R,Rb,Rm,Ym,beta,XYZ,x,i);
 end
 [A B C] = ABC(F,R,Rb,Rm,Ym,beta);
 [P D] = PD(A,B,C,beta,R,Rb);
-for k = 1:M
-    penalty(k) = norm(XYZ(k,:)-x,2)^2 - 4*R^2*(1 - cos(D(k)/R))/2;
+for i = 1:M
+    penalty(i) = norm(XYZ(i,:)-x,2)^2 - 4*R^2*(1 - cos(D(i)/R))/2;
 end
-beta0 = beta;
 if sum(penalty) < 1e-5
+    beta0(k,:) = beta;
     break;
 end
 end
+end
+emitter = emitter';
 end
---
+++ main_syn.m
@@ -0,0 +1,137 @@
+clear all
+R = 6371.2;
+plt = 0;
+if plt == 1
+    figure('color','k')
+    ha=axes('units','normalized','position',[0 0 1 1]);
+    uistack(ha,'down')
+    img=imread('background.jpg');
+    image(img)
+    colormap gray
+    set(ha,'handlevisibility','off','visible','off');
+    rotate3d on
+    hold on
+    %Add legend
+    %emitter
+    point1 = scatter3(0,0,0,50,'filled','r');
+    %sensors
+    point2 = scatter3(0,0,0,'filled','c');
+    %initial point
+    point3 = scatter3(0,0,0,50,'filled','b');
+    %recover location
+    point4 = scatter3(0,0,0,50,'k*');
+    %generated sequence
+    point5 = scatter3(0,0,0,5,'m');
+    %plot earth
+    npanels = 72;
+    alpha   = 1;
+    image_file = 'earth.jpg';
+    [x0, y0, z0] = ellipsoid(0, 0, 0, R, R, R);
+    globe = surf(x0, y0, -z0, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
+    cdata = imread(image_file);
+    set(gca, 'NextPlot','add', 'Visible','off');
+    set(globe, 'FaceColor', 'texturemap', 'CData', cdata, 'FaceAlpha', alpha, 'EdgeColor', 'none');
+    axis off;
+    axis equal;
+    axis auto;
+end
+M = 6;
+K = 4;
+N = M*(M-1)/2;
+Omega = covariance(1,M);
+inv_Omega = inv(Omega(1:M-1,1:M-1));
+delta = 0;
+Rm = 6650;
+Ym = 100;
+Rb = Rm - Ym;
+fc = 10;
+f = 15;
+F = f/fc;
+%perturbation on ionosphere
+P_Rm = Rm;
+P_Ym = Ym;
+P_Rb = P_Rm - P_Ym;
+P_fc = fc;
+P_f = f;
+P_F = P_f/P_fc;
+[max_dis,min_dis,upper] = beta_bound(M,F,R,Rb,Rm,Ym);
+[G] = generate_G(N,M);
+[emitter,XYZ,beta0] = generator(M,K,F,R,Rb,Rm,Ym,max_dis,min_dis);
+
+for k = 1:K
+    eval("emitter"+string(k)+" = emitter(:,"+string(k)+");");
+    eval("beta"+string(k)+" = beta0("+string(k)+",:);");
+end
+      
+
+tau = [];
+for i = 1:K
+    eval("tau"+string(i)+" = generate_tau(M,F,R,Rb,Rm,Ym,emitter"+string(i)+",XYZ);");
+    tau = [tau,eval("tau"+string(i)+"'")];
+end
+
+t_0 = [];
+for i = 1:K
+    [A B C] = ABC(F,R,Rb,Rm,Ym,eval("beta"+string(i)));
+    eval("[t"+string(i)+" D] = PD(A,B,C,beta"+string(i)+",R,Rb);");
+    t_0 = [t_0,eval("t"+string(i)+"'")];
+end
+
+t = zeros(M,K);
+for i = 1:M-1
+    eval("P_"+string(i)+" = permutation(K);");
+end
+
+tau_0 = tau(1:M-1,:);
+P_tau = [tau(1,:)*P_1;tau(2,:)*P_2;tau(3,:)*P_3;tau(4,:)*P_4;tau(5,:)*P_5];
+
+ini = '';
+for i = 1:M-1
+     ini = ini + "param.cut"+string(i)+"(:,:,1) = zeros(K,K);";
+end
+eval(ini);
+cvx_solver mosek
+for iter = 1:30
+    [P_tau0 param] = IP(M,G,t,P_tau,K,param);
+    obj = trace((G(1:M-1,1:M)*t - P_tau0)'*inv_Omega*(G(1:M-1,1:M)*t - P_tau0));
+    fprintf("obj:%2.8f K:%d\n",obj,K);
+    diag1 = trace((G(1:M-1,1:M)*t - P_tau0)'*inv_Omega*(G(1:M-1,1:M)*t - P_tau0));
+    [t_sum,obj_sum,location] = solve_GPGD(M,N,F,Rb,Rm,Ym,P_F,R,P_Rb,P_Rm,P_Ym,G,P_tau0,inv_Omega,upper,max_dis,min_dis,XYZ,plt,K);
+    diag2 = trace((G(1:M-1,1:M)*t_sum - P_tau0)'*inv_Omega*(G(1:M-1,1:M)*t_sum - P_tau0));
+    
+	t = t_sum;
+    index = find(obj_sum >= 1e1);
+    if isempty(index)
+        break
+    end
+    P_tau = P_tau0(:,index);
+	K_old = K;
+    K = size(P_tau,2);
+    if K ~= K_old
+        t = t_sum(:,index);
+        clear param
+        ini = '';
+        for i = 1:M-1
+             ini = ini + "param.cut"+string(i)+"(:,:,1) = zeros(K,K);";
+        end
+        eval(ini);
+    end
+end
+
+if plt == 1
+    scatter3(emitter1(1),emitter1(2),emitter1(3),50,'filled','r')
+    scatter3(emitter2(1),emitter2(2),emitter2(3),50,'filled','r')
+    scatter3(emitter3(1),emitter3(2),emitter3(3),50,'filled','r')
+    scatter3(emitter4(1),emitter4(2),emitter4(3),50,'filled','r')
+    scatter3(x(1),x(2),x(3),50,'k*')
+    %text(emitter(1),emitter(2),emitter(3),'e')
+end
+for i = 1:M
+    if plt == 1
+        scatter3(XYZ(i,1),XYZ(i,2),XYZ(i,3),'filled','c')
+        temp = strcat('s ',num2str(i));
+        text(XYZ(i,1),XYZ(i,2),XYZ(i,3),temp);
+    end
+    fprintf("Sensor %d:(%2.2f,%2.2f,%2.2f)\n",i,XYZ(i,1),XYZ(i,2),XYZ(i,3))
+end
+
