74368e72f955af75d7aa673d5f60be3b3dd268a7
samwong1993 <42926476+samwong1993@users.noreply.github.com>
2020-12-02 11:45:38 +0800

    update

--- LOS-model/del.m
+++ LOS-model/del.m
@@ -1,9 +1,9 @@
 function output = del(tau,del_tau)
-
-output = [];
-for i = 1:length(del_tau)
-    [~,index] = min(abs(tau(i,:) - del_tau(i)));
-    output(i,:) = setdiff(tau(i,:),tau(i,index));
-end
-
+    output = [];
+    for i = 1:length(del_tau)
+        [~,index] = min(abs(tau(i,:) - del_tau(i)));
+        output_temp = tau(i,:);
+        output_temp(index) = [];
+        output(i,:) = output_temp;
+    end
 end
